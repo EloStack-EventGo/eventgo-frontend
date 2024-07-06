b2f2fc69c9d0452a0a3c5082d36862f90031fb57
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-07-05 21:13:49 -0700

    updated profile icon options

--- app/components/AuthModal.js
+++ app/components/AuthModal.js
@@ -1,28 +1,28 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const AuthModal = ({ isVisible }) => {
   return (
     <div
       className={`absolute w-[180px] top-[40px] right-[-1px] bg-white border rounded-lg shadow-lg p-4 ${
-        isVisible ? 'block' : 'hidden'
+        isVisible ? "block" : "hidden"
       } tooltip`}
     >
-      <span className="flex flex-row justify-center text-white text-center text-md">
-        <Link href={'/register'}>
+      <span className="flex flex-row justify-center text-black text-center text-md">
+        <Link href={"/user-profile"}>
           <p className="hover:font-bold w-[70px] text-right hover:text-sky-700">
-            Register
+            My Profile
           </p>
         </Link>
         <span className="px-1">/</span>
-        <Link href={'/login'}>
+        <Link href={"/logout"}>
           <p className="hover:font-bold w-[48px] text-left hover:text-sky-700">
-            Login
+            Logout
           </p>
         </Link>
       </span>
     </div>
-  )
-}
+  );
+};
 
-export default AuthModal
+export default AuthModal;
